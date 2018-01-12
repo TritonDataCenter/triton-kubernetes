cab3addbb923f167c3f5f9664d8004690c48b563
Nima Jalali <nima@jalali.net>
2018-01-11 17:18:06 -0800

    add get cmd to describe cluster managers

---
+++ cmd/get.go
@@ -0,0 +1,61 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/joyent/triton-kubernetes/get"
+
+	"github.com/spf13/cobra"
+)
+
+// getCmd represents the get command
+var getCmd = &cobra.Command{
+	Use:       "get [manager]",
+	Short:     "Display resource information",
+	Long:      `Get allows you to get cluster manager details.`,
+	ValidArgs: []string{"manager"},
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return errors.New(`"triton-kubernetes get" requires one argument`)
+		}
+
+		for _, validArg := range cmd.ValidArgs {
+			if validArg == args[0] {
+				return nil
+			}
+		}
+
+		return fmt.Errorf(`invalid argument "%s" for "triton-kubernetes get"`, args[0])
+	},
+	Run: getCmdFunc,
+}
+
+func getCmdFunc(cmd *cobra.Command, args []string) {
+	getType := args[0]
+	switch getType {
+	case "manager":
+		fmt.Println("get manager called")
+		err := get.GetManager()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(getCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// getCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+}
---
+++ get/manager.go
@@ -0,0 +1,110 @@
+package get
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/Jeffail/gabs"
+	"github.com/joyent/triton-kubernetes/remote"
+	"github.com/joyent/triton-kubernetes/shell"
+	"github.com/joyent/triton-kubernetes/util"
+
+	"github.com/manifoldco/promptui"
+	"github.com/spf13/viper"
+)
+
+func GetManager() error {
+	tritonAccount, tritonKeyPath, tritonKeyID, tritonURL, mantaURL, err := util.GetTritonAccountVariables()
+	if err != nil {
+		return err
+	}
+
+	remoteClusterManagerState, err := remote.NewRemoteClusterManagerStateManta(tritonAccount, tritonKeyPath, tritonKeyID, tritonURL, mantaURL)
+	if err != nil {
+		return err
+	}
+
+	clusterManagers, err := remoteClusterManagerState.List()
+	if err != nil {
+		return err
+	}
+
+	if len(clusterManagers) == 0 {
+		return fmt.Errorf("No cluster managers.")
+	}
+
+	selectedClusterManager := ""
+	if viper.IsSet("cluster_manager") {
+		selectedClusterManager = viper.GetString("cluster_manager")
+	} else {
+		prompt := promptui.Select{
+			Label: "Cluster Manager",
+			Items: clusterManagers,
+		}
+
+		_, value, err := prompt.Run()
+		if err != nil {
+			return err
+		}
+
+		selectedClusterManager = value
+	}
+
+	// Verify selected cluster manager exists
+	found := false
+	for _, clusterManager := range clusterManagers {
+		if selectedClusterManager == clusterManager {
+			found = true
+			break
+		}
+	}
+	if !found {
+		return fmt.Errorf("Selected cluster manager '%s' does not exist.", selectedClusterManager)
+	}
+
+	// Load current cluster manager config
+	clusterManagerTerraformConfigBytes, err := remoteClusterManagerState.GetTerraformConfig(selectedClusterManager)
+	if err != nil {
+		return err
+	}
+
+	clusterManagerTerraformConfig, err := gabs.ParseJSON(clusterManagerTerraformConfigBytes)
+	if err != nil {
+		return err
+	}
+
+	// Create a temporary directory
+	tempDir, err := ioutil.TempDir("", "triton-kubernetes-")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(tempDir)
+
+	// Save the terraform config to the temporary directory
+	jsonBytes := []byte(clusterManagerTerraformConfig.StringIndent("", "\t"))
+	jsonPath := fmt.Sprintf("%s/%s", tempDir, "main.tf.json")
+	err = ioutil.WriteFile(jsonPath, jsonBytes, 0644)
+	if err != nil {
+		return err
+	}
+
+	// Use temporary directory as working directory
+	shellOptions := shell.ShellOptions{
+		WorkingDir: tempDir,
+	}
+
+	// Run terraform init
+	err = shell.RunShellCommand(&shellOptions, "terraform", "init", "-force-copy")
+	if err != nil {
+		return err
+	}
+
+	// Run terraform output
+	err = shell.RunShellCommand(&shellOptions, "terraform", "output", "-module", "cluster-manager")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
