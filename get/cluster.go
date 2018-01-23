7001570d7d542953ec95c493dedbbf2bbd410f54
Chris Guevara <chrismguevara@gmail.com>
2018-01-23 10:46:19 -0800

    Add get cluster command

---
+++ get/cluster.go
@@ -0,0 +1,135 @@
+package get
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+
+	"github.com/joyent/triton-kubernetes/backend"
+	"github.com/joyent/triton-kubernetes/shell"
+
+	"github.com/manifoldco/promptui"
+	"github.com/spf13/viper"
+)
+
+func GetCluster(remoteBackend backend.Backend) error {
+	clusterManagers, err := remoteBackend.States()
+	if err != nil {
+		return err
+	}
+
+	if len(clusterManagers) == 0 {
+		return fmt.Errorf("No cluster managers.")
+	}
+
+	selectedClusterManager := ""
+	if viper.IsSet("cluster_manager") {
+		selectedClusterManager = viper.GetString("cluster_manager")
+	} else {
+		prompt := promptui.Select{
+			Label: "Cluster Manager",
+			Items: clusterManagers,
+		}
+
+		_, value, err := prompt.Run()
+		if err != nil {
+			return err
+		}
+
+		selectedClusterManager = value
+	}
+
+	// Verify selected cluster manager exists
+	found := false
+	for _, clusterManager := range clusterManagers {
+		if selectedClusterManager == clusterManager {
+			found = true
+			break
+		}
+	}
+	if !found {
+		return fmt.Errorf("Selected cluster manager '%s' does not exist.", selectedClusterManager)
+	}
+
+	state, err := remoteBackend.State(selectedClusterManager)
+	if err != nil {
+		return err
+	}
+
+	// Get existing clusters
+	clusters, err := state.Clusters()
+	if err != nil {
+		return err
+	}
+
+	if len(clusters) == 0 {
+		return fmt.Errorf("No clusters.")
+	}
+
+	selectedClusterKey := ""
+	if viper.IsSet("cluster_name") {
+		clusterName := viper.GetString("cluster_name")
+		clusterKey, ok := clusters[clusterName]
+		if !ok {
+			return fmt.Errorf("A cluster named '%s', does not exist.", clusterName)
+		}
+
+		selectedClusterKey = clusterKey
+	} else {
+		clusterNames := make([]string, 0, len(clusters))
+		for name := range clusters {
+			clusterNames = append(clusterNames, name)
+		}
+		sort.Strings(clusterNames)
+		prompt := promptui.Select{
+			Label: "Cluster to view",
+			Items: clusterNames,
+			Templates: &promptui.SelectTemplates{
+				Label:    "{{ . }}?",
+				Active:   fmt.Sprintf("%s {{ . | underline }}", promptui.IconSelect),
+				Inactive: " {{ . }}",
+				Selected: fmt.Sprintf(`{{ "%s" | green }} {{ "Cluster:" | bold}} {{ . }}`, promptui.IconGood),
+			},
+		}
+
+		_, value, err := prompt.Run()
+		if err != nil {
+			return err
+		}
+		selectedClusterKey = clusters[value]
+	}
+
+	// Create a temporary directory
+	tempDir, err := ioutil.TempDir("", "triton-kubernetes-")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(tempDir)
+
+	// Save the terraform config to the temporary directory
+	jsonPath := fmt.Sprintf("%s/%s", tempDir, "main.tf.json")
+	err = ioutil.WriteFile(jsonPath, state.Bytes(), 0644)
+	if err != nil {
+		return err
+	}
+
+	// Use temporary directory as working directory
+	shellOptions := shell.ShellOptions{
+		WorkingDir: tempDir,
+	}
+
+	// Run terraform init
+	err = shell.RunShellCommand(&shellOptions, "terraform", "init", "-force-copy")
+	if err != nil {
+		return err
+	}
+
+	// Run terraform output
+	err = shell.RunShellCommand(&shellOptions, "terraform", "output", "-module", selectedClusterKey)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
