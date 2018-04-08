816c52f9d8641fe7bc54849d70995686b56d2c3c
Chris Guevara <chrismguevara@gmail.com>
2018-04-08 13:08:48 -0700

    Support Azure disks during node creation

--- terraform/modules/azure-rancher-k8s-host/main.tf
+++ terraform/modules/azure-rancher-k8s-host/main.tf
@@ -31,6 +31,8 @@ data "template_file" "install_rancher_agent" {
     rancher_registry          = "${var.rancher_registry}"
     rancher_registry_username = "${var.rancher_registry_username}"
     rancher_registry_password = "${var.rancher_registry_password}"
+
+    disk_mount_path = "${var.azure_disk_mount_path}"
   }
 }
 
@@ -54,6 +56,17 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "dynamic"
     public_ip_address_id          = "${azurerm_public_ip.public_ip.id}"
   }
+}
+
+resource "azurerm_managed_disk" "host_disk" {
+  count = "${var.azure_disk_mount_path == "" ? 0 : 1}"
+
+  name                 = "${var.hostname}-managed-disk"
+  location             = "${var.azure_location}"
+  resource_group_name  = "${var.azure_resource_group_name}"
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "${var.azure_disk_size}"
 }
 
 resource "azurerm_virtual_machine" "host" {
@@ -81,11 +94,12 @@ resource "azurerm_virtual_machine" "host" {
   }
 
   storage_data_disk {
-    name              = "${var.hostname}-datadisk"
+    name              = "${element(coalescelist(azurerm_managed_disk.host_disk.*.name, list("${var.hostname}-datadisk")), 0)}"
+    managed_disk_id   = "${element(coalescelist(azurerm_managed_disk.host_disk.*.id, list("")), 0)}"
     managed_disk_type = "Standard_LRS"
-    create_option     = "Empty"
+    create_option     = "${var.azure_disk_mount_path == "" ? "Empty" : "Attach"}"
     lun               = 0
-    disk_size_gb      = "1023"
+    disk_size_gb      = "${element(coalescelist(azurerm_managed_disk.host_disk.*.disk_size_gb, list("1023")), 0)}"
   }
 
   os_profile {
