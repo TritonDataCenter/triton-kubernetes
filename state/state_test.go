9697ef85795c5a39025875e85e3d1bee42aec9a2
Nirav Patel <niravpatel27@gmail.com>
2018-04-17 16:56:41 -0500

    refine error

--- state/state_test.go
+++ state/state_test.go
@@ -13,7 +13,7 @@ func TestGet(t *testing.T) {
 
 	dumyKey := stateObj.Get("config.triton.key")
 	if dumyKey != "55fd4s" {
-		t.Errorf("value in state object, got: %s, want: %s.", dumyKey, "")
+		t.Errorf("value in state object, got: %s, want: %s.", dumyKey, "55fd4s")
 	}
 }
 
