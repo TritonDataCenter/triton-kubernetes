c99cc6007752e5d8de816eddcdb5c017d8eff9d2
Sachin Gupta <sachingupta@joyentmac12331.local>
2018-04-19 20:16:18 -0700

    Some basic testing for the backend prompt cli, mostly covering the error messages for non-interactive mode

---
+++ util/backend_prompt_test.go
@@ -0,0 +1,86 @@
+package util
+
+import (
+	"testing"
+	"github.com/spf13/viper"
+)
+
+
+func TestBackendPromptWithUnsupportedBackendProviderNonInteractiveMode(t *testing.T) {
+	viper.Set("non-interactive", true)
+	viper.Set("backend_provider", "S3")
+
+	_,err:=PromptForBackend()
+
+	expected:= "Unsupported backend provider 'S3'"
+
+	if err.Error() != expected {
+		t.Error(err)
+	}
+}
+
+
+func TestBackendPromptWithNoTritonAccountNonInteractiveMode(t *testing.T) {
+	viper.Set("non-interactive", true)
+	viper.Set("backend_provider", "manta")
+
+	_,err:=PromptForBackend()
+
+	expected:= "triton_account must be specified"
+
+	if err.Error() != expected {
+		t.Error(err)
+	}
+}
+
+func TestBackendPromptWithNoritonSSHKeyPathNonInteractiveMode(t *testing.T) {
+	viper.Set("triton_account", "xyz")
+
+	_,err:=PromptForBackend()
+
+	expected:= "triton_key_path must be specified"
+
+	if err.Error() != expected {
+		t.Error(err)
+	}
+}
+
+func TestBackendPromptWithInvalidTritonSSHKeyPathInteractive(t *testing.T) {
+	viper.Set("non-interactive", false)
+	viper.Set("triton_key_path", "")
+
+	_,err:=PromptForBackend()
+
+	expected:= "Unable to read private key: open : no such file or directory"
+
+	if err.Error() != expected {
+		t.Error(err)
+	}
+}
+
+func TestNoTritonURLForNonInteractiveMode (t *testing.T) {
+	viper.Set("non-interactive", true)
+	viper.Set("triton_key_path", "~/.ssh/id_rsa")
+
+	_,err := PromptForBackend()
+
+	expected := "triton_url must be specified"
+
+	if err.Error() != expected {
+		t.Error(err)
+	}
+
+}
+
+func TestNoMantaURLForNonInteractiveMode (t *testing.T) {
+	viper.Set("triton_url", "xyz.triton.com")
+
+	_,err := PromptForBackend()
+
+	expected := "manta_url must be specified"
+
+	if err.Error() != expected {
+		t.Error(err)
+	}
+
+}
