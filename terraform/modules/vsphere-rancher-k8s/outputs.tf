9628138fb740d00b7b57af4120288e8ef1629839
Nima Jalali <nima@jalali.net>
2018-08-13 01:35:21 -0700

    use lookup interpolation for external data source output

--- terraform/modules/vsphere-rancher-k8s/outputs.tf
+++ terraform/modules/vsphere-rancher-k8s/outputs.tf
@@ -1,13 +1,13 @@
 output "rancher_cluster_id" {
-  value = "${data.external.rancher_cluster.result.cluster_id}"
+  value = "${lookup(data.external.rancher_cluster.result, "cluster_id")}"
 }
 
 output "rancher_cluster_registration_token" {
-  value = "${data.external.rancher_cluster.result.registration_token}"
+  value = "${lookup(data.external.rancher_cluster.result, "registration_token")}"
 }
 
 output "rancher_cluster_ca_checksum" {
-  value = "${data.external.rancher_cluster.result.ca_checksum}"
+  value = "${lookup(data.external.rancher_cluster.result, "ca_checksum")}"
 }
 
 output "vsphere_datacenter_name" {
