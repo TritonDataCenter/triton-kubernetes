34b9ecac2d04484844ad32f33c8334fe094caa8e
Pedro Palazón Candel <pedro@joyent.com>
2020-05-18 19:26:48 +0200

    Migrate from deps to Go 1.11+ modules.

--- vendor/github.com/joyent/triton-go/scripts/lint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-result=0
-for pkg in $(go list ./...| grep -Ev 'vendor|examples|testutils')
-do
-    golint -set_exit_status "$pkg" || result=1
-    go vet "$pkg" || result=1
-done
-exit $result
