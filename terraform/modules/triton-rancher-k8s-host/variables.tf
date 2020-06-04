d751b96485f079bb6d3125e7434f44ce699742b4
Pedro Palazón Candel <pedro@joyent.com>
2020-06-04 16:58:21 +0200

    Update Docker from 17.03 to 19.03

--- terraform/modules/triton-rancher-k8s-host/variables.tf
+++ terraform/modules/triton-rancher-k8s-host/variables.tf
@@ -36,7 +36,7 @@ variable "rancher_registry_password" {
 }
 
 variable "docker_engine_install_url" {
-  default     = "https://raw.githubusercontent.com/joyent/triton-kubernetes/kusor/scripts/docker/19.03.sh"
+  default     = "https://raw.githubusercontent.com/joyent/triton-kubernetes/master/scripts/docker/19.03.sh"
   description = "The URL to the shell script to install the docker engine."
 }
 
